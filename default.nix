6f69d9f67e0fc9cd5f1766b614b4698f8e79ab6b
karetsu <raisedbyfinches@hotmail.co.uk>
2019-09-20 14:28:41 +0100

    bug: got fed up of xmessage, project now not in ~/.xmonad

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@
 let
 
   haskellPackages = pkgs.haskell.packages.${compiler};
-  drv = haskellPackages.callCabal2nix "aloysius" (builtins.path {path = "/home/aloysius/.xmonad"; name="xmonad-aloysius";}) {};
+  drv = haskellPackages.callCabal2nix "aloysius" (builtins.path {path = "/home/aloysius/Projects/haskell/mine/xmonad"; name="xmonad-aloysius";}) {};
 
 in
   {
