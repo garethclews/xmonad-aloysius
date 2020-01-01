7739511068f20d3c7f0aadafa4767ed0c1d06e05
karetsu <raisedbyfinches@hotmail.co.uk>
2020-01-01 15:32:48 +0000

    :fire: themes now get applied across all parts

--- shell.nix
+++ shell.nix
@@ -1,42 +1,39 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+{ nixpkgs ? import <nixpkgs> { }, compiler ? "default", doBenchmark ? false }:
 
 let
 
   inherit (nixpkgs) pkgs;
 
-  f = { mkDerivation, base, Cabal, cabal-install, containers, stdenv, X11, xmonad
-      , xmonad-contrib, xmonad-extras, dbus, utf8-string
-      }:
-      mkDerivation {
-        pname = "aloysius";
-        version = "1.0.0.1";
-        src = ./.;
-        isLibrary = false;
-        isExecutable = true;
-        executableHaskellDepends = [ base
-                                     Cabal
-                                     cabal-install
-                                     containers
-                                     dbus
-                                     X11
-                                     xmonad
-                                     xmonad-contrib
-                                     xmonad-extras
-                                     utf8-string
-                                   ];
-        homepage = "https://gitlab.com/karetsu/xmonad-aloysius";
-        description = "xmonad config";
-        license = stdenv.lib.licenses.mit;
-      };
+  f = { mkDerivation, base, Cabal, cabal-install, containers, stdenv, X11
+    , xmonad, xmonad-contrib, xmonad-extras }:
+    mkDerivation {
+      pname = "aloysius";
+      version = "1.0.0.1";
+      src = ./.;
+      isLibrary = false;
+      isExecutable = true;
+      executableHaskellDepends = [
+        base
+        Cabal
+        cabal-install
+        containers
+        X11
+        xmonad
+        xmonad-contrib
+        xmonad-extras
+      ];
+      homepage = "https://gitlab.com/karetsu/xmonad-aloysius";
+      description = "xmonad config";
+      license = stdenv.lib.licenses.mit;
+    };
 
-  haskellPackages = if compiler == "default"
-                       then pkgs.haskellPackages
-                       else pkgs.haskell.packages.${compiler};
+  haskellPackages = if compiler == "default" then
+    pkgs.haskellPackages
+  else
+    pkgs.haskell.packages.${compiler};
 
   variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
 
-  drv = variant (haskellPackages.callPackage f {});
+  drv = variant (haskellPackages.callPackage f { });
 
-in
-
-  if pkgs.lib.inNixShell then drv.env else drv
+in if pkgs.lib.inNixShell then drv.env else drv
