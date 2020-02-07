af0b37b6e007b96d65d8705e248cc0efb73fb211
karetsu <914590-karetsu@users.noreply.gitlab.com>
2020-02-07 00:58:20 +0000

    version: 1.0.4.0 -> 1.1.1.0

--- shell.nix
+++ shell.nix
@@ -8,7 +8,7 @@ let
     , xmonad, xmonad-contrib, xmonad-extras }:
     mkDerivation {
       pname = "aloysius";
-      version = "1.0.4.0";
+      version = "1.1.1.0";
       src = ./.;
       isLibrary = false;
       isExecutable = true;
